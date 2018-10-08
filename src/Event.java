5ecb7bca9161c5f91afd70834b8e2c22feffd975
NickMooon <43826016+NickMooon@users.noreply.github.com>
2018-10-08 10:11:38 +0300

    popopo

--- src/Event.java
+++ src/Event.java
@@ -4,19 +4,20 @@ import Users.RegistratedUsers;
 import java.util.ArrayList;
 
 public class Event {
+    static Event festival = new Event();
     FestivalInfo rockFestivalInfo = new FestivalInfo();
     ArrayList<RegistratedUsers> users = new ArrayList<>();
-    try{
-        
-    }
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
+        try {
+            festival.registration();
+        }
+        catch (FullUserException e){
+            System.out.println("Регистрация невозможна. Все билеты проданы.");
+        }
     }
-    public void Registration() throws FullUserException{
+    public void registration() throws FullUserException{
+        if (users.size()>rockFestivalInfo.maxUsers)  throw new FullUserException();
         users.add(new RegistratedUsers());
-        if (users.size()>rockFestivalInfo.maxUsers) failRegistration();
-    }
-    public void failRegistration() throws FullUserException {
-        throw new FullUserException();
     }
 }
